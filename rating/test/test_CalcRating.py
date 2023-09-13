32c254f4148bfe86acd4e8e5e066b32b4ececcc2
Alex <alexanderbor0134@mail.ru>
2023-09-13 13:46:02 +0300

    test commit

---
+++ rating/test/test_CalcRating.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from rating.src.Types import DataType
+from rating.src.CalcRating import CalcRating
+import pytest
+
+RatingType = dict[str, float]
+
+
+class TestCalcRating:
+    @pytest.fixture()
+    def input_data(self) -> tuple[DataType, RatingType]:
+        data: DataType = {
+            "Абрамов Петр Сергеевич":
+                [
+                    ("математика", 80),
+                    ("русский язык", 76),
+                    ("программирование", 100)
+                ],
+            "Петров Игорь Владимирович":
+                [
+                    ("математика", 61),
+                    ("русский язык", 70),
+                    ("программирование", 78),
+                    ("литература", 97)
+                ]
+        }
+
+        rating_scores: RatingType = {
+            "Абрамов Петр Сергеевич": 85.3333,
+            "Петров Игорь Владимирович": 79.0000
+        }
+
+        return  data, rating_scores
+
+    def test_init_calc_rating(self, input_data: tuple[DataType, RatingType]) -> None:
+        calc_rating = CalcRating(input_data[0])
+        assert input_data[0] == calc_rating.data
+
+    def test_calc(self, input_data: tuple[DataType, RatingType]) -> None:
+        rating = CalcRating(input_data[0]).calc()
+        for student in rating.keys():
+            rating_score = rating[student]
+            assert pytest.approx(rating_score, abs=0.001) == input_data[1][student]
