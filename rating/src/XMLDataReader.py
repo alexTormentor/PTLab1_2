7781e56fc0b173b79b8dedc017a65c5e30ed9d27
Alex <alexanderbor0134@mail.ru>
2023-09-20 20:13:32 +0300

    added XML data reader and old main.py

---
+++ rating/src/XMLDataReader.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from Types import DataType
+from DataReader import DataReader
+import xml.etree.ElementTree as ET
+
+class XMLDataReader(DataReader):
+    def __init__(self) -> None:
+        self.key: str = ""
+        self.students: DataType = {}
+
+    def read(self, path: str) -> DataType:
+        with open(path, encoding='utf-8') as file:
+            tree = ET.parse(file)
+            root = tree.getroot()
+            for student_elem in root.findall('student'):
+                self.key = student_elem.find('name').text
+                self.students[self.key] = []
+                for subject_elem in student_elem.findall('subject'):
+                    subj = subject_elem.find('name').text
+                    score = int(subject_elem.find('score').text)
+                    self.students[self.key].append((subj, score))
+        return self.students
