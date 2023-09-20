e804615ebfeffb204e03fe0f4631f7c5ac0d1917
Alex <alexanderbor0134@mail.ru>
2023-09-20 20:04:12 +0300

    TEST branch has modify main.py

--- rating/src/main.py
+++ rating/src/main.py
@@ -4,29 +4,38 @@ import sys
 
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
+from XMLDataReader import XMLDataReader  # Import the XMLDataReader class
 
 
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
     parser.add_argument("-p", dest="path", type=str, required=True,
-                        help="Path tp datafile")
+                        help="Path to datafile")
+    parser.add_argument("-f", dest="format", type=str, required=True,
+                        choices=["text", "xml"], help="Data format (text or xml)")
     args = parser.parse_args(args)
-    return args.path
-
+    return args.path, args.format
 
 def main():
-    path = get_path_from_arguments(sys.argv[1:])
+    path, data_format = get_path_from_arguments(sys.argv[1:])
 
-    reader = TextDataReader()
+    if data_format == "text":
+        reader = TextDataReader()
+    elif data_format == "xml":
+        reader = XMLDataReader()
+    else:
+        print("Invalid data format. Use 'text' or 'xml'.")
+        return
+
     students = reader.read(path)
     print("Students: ", students)
 
     rating = CalcRating(students).calc()
     print("Rating: ", rating)
 
-
 if __name__ == "__main__":
     main()
+
 
 
 # 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 - токен гитхаба
