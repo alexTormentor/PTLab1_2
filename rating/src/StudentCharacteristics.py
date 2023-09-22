3f8da119421567e6175bf4a205ad27c07922343c
Alex <alexanderbor0134@mail.ru>
2023-09-22 17:24:29 +0300

    Quartile calculation for XML

---
+++ rating/src/StudentCharacteristics.py
@@ -0,0 +1,20 @@
+from typing import List
+from CalcRating import RatingType
+import numpy as np
+
+
+class StudentCharacteristics:
+    def __init__(self, rating: RatingType) -> None:
+        self.rating = rating
+
+    def calculate_first_quartile(self) -> List[str]:
+        # получаем словарь
+        ratings = list(self.rating.values())
+
+        # вычисляем квартиль
+        first_quartile = np.percentile(ratings, 25)
+
+        # студенты у которых рейтинг в первом квартиле
+        first_quartile_students = [student for student, student_rating in self.rating.items() if student_rating <= first_quartile]
+
+        return first_quartile_students
